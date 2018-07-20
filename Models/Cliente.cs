5eb4f68df10de74b4cf0b6bab7121d6e3d61943e
Guilherme Viana <guilhermevp1996@hotmail.com>
2018-07-20 09:00:32 -0300

    Update Cliente.cs

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -7,17 +7,19 @@ namespace Brainsman.Models
 {
     public class Cliente
     {
+        //Atributos da classe Cliente
         public string nome { get; set; }
         public int idade { get; set; }
         public string cpf { get; set; }
         public string apelido { get; set; }
         public string email { get; set; }
         public string senha { get; set; }
-
+        //Construtor padrão
         public Cliente()
         {
 
         } 
+        //Construtor de classe
         public Cliente(string nome, int idade, string cpf, string apelido, string email, string senha)
         {
             this.nome = nome;
