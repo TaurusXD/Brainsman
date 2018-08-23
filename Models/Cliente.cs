1fd86dbb69dc5cc5e68a4a98a501438af8c10b56
Guilherme Viana <guilhermevp1996@hotmail.com>
2018-08-23 13:54:28 -0300

    Annotations
    I inserted the annotations into the client class

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -6,9 +6,15 @@ using System.Web;
 
 namespace Brainsman.Models
 {
+    [Table("CLIENTE")]
     public class Cliente
     {
 <<<<<<< HEAD
+    
+        [Key]
+        [Coloumn(Name = "COD")]
+        public int cod {get; set;}
+        
         [Required(ErrorMessage="Nome obrigatório")]
         [StringLength(50, MinimumLength =3,ErrorMessage ="O nome deve ter, no  mínimo 3 letras.")]
         [Display(Name="Nome:")]
@@ -16,17 +22,22 @@ namespace Brainsman.Models
         //Atributos da classe Cliente
 >>>>>>> 5eb4f68df10de74b4cf0b6bab7121d6e3d61943e
         public string nome { get; set; }
+        [Coloumn ("IDADE")]
         [Required(ErrorMessage = "Idade obrigatória.")]
         [StringLength(3, MinimumLength = 1, ErrorMessage = "Idade incorreta!")]
         [Display(Name = "Idade:")]
         public int idade { get; set; }
+        [Coloumn ("CPF")]
         [Required(ErrorMessage = "CPF obrigatório")]
         [StringLength(11, MinimumLength = 11, ErrorMessage = "CPF inválido.")]
         [Display(Name = "CPF:")]
         public string cpf { get; set; }
+        [Coloumn ("APELIDO")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "O apelido deve ter, no  mínimo 3 letras.")]
         [Display(Name = "Apelido:")]
         public string apelido { get; set; }
+        [Coloumn ("EMAIL")]
+        [Display (Name = "Email")]
         public string email { get; set; }
         public string senha { get; set; }
         //Construtor padrão
